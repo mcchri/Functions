9f8013ba90ee0a44a99f9c82a6e988e7cf20a987
mcchri <112865223+mcchri@users.noreply.github.com>
2023-01-13 12:26:20 +0000

    Add files via upload

--- funct3.js
+++ funct3.js
@@ -10,29 +10,82 @@ function mean(ar){
 }
 
 mean([1,2,3,4,5]);
+//--------------------------------------------------- MODE
+var x = [1,2,3,4,5,6,6,5,6,5];
 
-function mode(ar){
-    let mode = 0;
-    let mode_num = [];
-    count = 0;
-    for(j = 0;j<ar.length;j++){
-        let one = ar[j];
-    for(i = 0;i<ar.length;i++){
-        if(one == ar[i]){
-            count++;
+call_me(x);
+
+
+function call_me(params){
+    let mode = [];
+    let modec = 0;
+    let moodeb = 0;
+    let mode_num = -1;
+    for(let i = 0;i<params.length;i++){
+        let count = -1;
+        for(let j = 0;j<params.length;j++){
+            if(params[i] == params[j]){
+                
+                count++;
+            }
+            
+        }
+        document.write(count);
+        if(count > mode_num){
+            mode_num = count;
+            mode = [];
+            modeb = params[i];
+            
+        }else if(count == mode_num && modeb != params[i] && modec != params[i]){
+            mode.push(params[i]);
+            modec = params[i];
         }
     }
-    if(count > mode){
-        mode == count;
-        mode
-    }else if(count == mode){
-        mode_num += one;
+    mode.push(modeb);
+    for(let k = 0; k<mode.length;k++){
+        console.log(mode[k]);
     }
-    //for(let k = 0; k < mode_num.length;k++){
-        console.log(mode_num[1]);
-    //}
 }
 
+//----------------------------------------------median
+var y = [1,2,3,4,5,6,6,5,6,5];
+
+median(y);
+
+function median(params){
+    let num = 0;
+    let le = params.length
+    if(params.length % 2 == 0){
+        num = ((le / 2) + ((le / 2) + 1)) / 2;
+    }else if(le % 2 == 1){
+        num = le / 2;
+    }
+    console.log(num);
 }
 
-mode([1,2,3,4,5,5,4,3]);
+//-----------------------------------frequency
+
+var z = [1,2,3,4,5,6,6,5,6,5];
+
+freq(z);
+
+function freq(params){
+    params.sort(function(a, b){return a - b});
+    let sa = null;
+    for(let i = 0;i<params.length;i++){
+        let count = 0;
+        for(let j = 0;j<params.length;j++){
+            if(params[i] == params[j]){
+                
+                count++;
+            }
+            
+        }
+        if(sa != params[i]){
+            sa = params[i];
+            console.log(params[i] + " : " + count);
+        }
+    }
+}
+
+
